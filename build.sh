d5f87ddd42606f579fa0e9c35be05b6814aad703
Giulio Lyons <24858993+GiulioLyons@users.noreply.github.com>
2025-02-27 13:06:17 +0100

    remove gradle

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+PROJECT_NAME="zygisk_example"
+PROJECT_DIR=$(pwd)
+OUTDIR="output"
+TEMPLATE="template/magisk_module"
+NDK_PATH=/home/w00t/coding/android/android-ndk-r27c
+PATH=$PATH:$NDK_PATH
+
+
+if [ -d "$OUTDIR" ]; then
+    rm -rf $OUTDIR
+    mkdir $OUTDIR
+fi
+
+cd module
+ndk-build -j4
+cd ..
+
+exit
+
+cp -R $TEMPLATE $OUTDIR/$PROJECT_NAME
+cp module.prop $OUTDIR/$PROJECT_NAME
+
+mkdir -p $OUTDIR/$PROJECT_NAME/zygisk
+
+for arch in $(ls module/libs); do cp module/libs/$arch/*.so $OUTDIR/$PROJECT_NAME/zygisk/$arch.so; done
+
+cd $OUTDIR/$PROJECT_NAME
+
+zip -r ../$PROJECT_NAME.zip ./*
+
+cd $PROJECT_DIR
+
+adb push $OUTDIR/$PROJECT_NAME.zip /data/local/tmp/
+
+adb shell su -c magisk --install-module /data/local/tmp/$PROJECT_NAME.zip
+
+adb shell reboot
+
+
